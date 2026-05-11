3e7c8b570cf357b7833d5cf1ca964f51879915da
n-j-m06 <menonniranjanj@gmail.com>
2026-05-11 13:58:33 +0530

    SUCCESS: Backend is running with functional folder structure

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
+# Removed "backend." from the start of the imports
 from app.models.database import init_db
-from app.api import auth # Import your auth file
+from app.api import auth 
 
 app = FastAPI(title="WaitSafe API")
 
 @app.on_event("startup")
 def on_startup():
     init_db()
+    print("Database connected and tables created!")
 
 # This tells FastAPI to include the signup routes
 app.include_router(auth.router)
